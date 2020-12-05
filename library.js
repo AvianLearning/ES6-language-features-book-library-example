b71e90b662ae3eff8773568b60556b24cef77bc2
AvianLearning <iaradcliffe@hotmail.com>
2020-12-05 14:34:51 +0000

    Refactored printInventory method and tested method works

--- library.js
+++ library.js
@@ -17,10 +17,16 @@ class Library {
 
   printInventory() {
     this.books.forEach((book) => {
-      console.log(`${book.title} by ${book.author}`);
+      const {author, title, genre} = book;
+      console.log(`${title} by ${author}`);
     });
   };
 
 };
 
 export default Library;
+
+// In library.js refactor the printInventory method using destructuring. 
+// Inside of the callback that we're passing to forEach unpack the fields title and author from the parameter book, 
+// so that they can be accessed directly via the variables title and author, 
+// rather than having to access them through book.title and book.author.
